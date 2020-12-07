7ee589b8e05d36d2688099f813f0a0f386fe770a
ejguan123 <ej_guan@tk3c.com>
2020-12-07 14:31:06 +0800

    修正本機圖片路徑

--- event/js/replace.js
+++ event/js/replace.js
@@ -22,9 +22,10 @@ var location = window.location.pathname.split('/');
             } else {
                 //圖片使用簡短網址: /images/xxxx.png (要到正式機才會抓的到圖片，不然在本機專案資料夾不同會顯示不出來)
                 if (imageUrl.src.indexOf("https://www.cdn-tkec.tw") < 0 && 
-                    imageUrl.src.indexOf('/images') > -1 &&
+                    newImagePath.indexOf('/images') > -1 &&
                     location[2] != undefined) {
-                    imageUrl.src = newUrl + imageStr + '/' + location[2] + newImagePath;
+                    var localImage = newImagePath.replace('./images','/images');
+                    imageUrl.src = newUrl + imageStr + '/' + location[2] + localImage;
                 }
             }
         }
