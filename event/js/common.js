4052a9143de5c2e960e23e65ca83128b3ed049bc
ejguan123 <ej_guan@tk3c.com>
2020-12-30 14:08:48 +0800

    modfiy scroll to show varble

--- event/js/common.js
+++ event/js/common.js
@@ -161,6 +161,7 @@ const addLeftAside = () => {
 
 //右側選單滑鼠滾到後顯示
 const scrollToShow = (type = true) =>  {
+    let rightbtn = $('body').find('.rightbtn');
     if (type !== false) {
         $(window).scroll(function(){
             const scrollTop = $(window).scrollTop();
