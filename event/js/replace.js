0ba7b9d63e710ab2c1fab255eadb2e6e9279a6b8
ejguan123 <ej_guan@tk3c.com>
2021-01-19 16:46:35 +0800

    modify replace image url

--- event/js/replace.js
+++ event/js/replace.js
@@ -22,11 +22,11 @@ var location = window.location.pathname.split('/');
                     }
                 } else {
                     //圖片使用簡短網址: /images/xxxx.png (要到正式機才會抓的到圖片，不然在本機專案資料夾不同會顯示不出來)
-                    if (this.src.indexOf("https://www.cdn-tkec.tw") < 0 && 
+                    if (imageUrl.src.indexOf("https://www.cdn-tkec.tw") < 0 && 
                         newImagePath.indexOf('/images') > -1 &&
                         location[2] != undefined) {
                         var localImage = newImagePath.replace('./images','/images');
-                        this.src = newUrl + imageStr + '/' + location[2] + localImage;
+                        imageUrl.src = newUrl + imageStr + '/' + location[2] + localImage;
                     }
                 }
             }
