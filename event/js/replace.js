d944545cdfb5b7625a9b793bd3724e1c2b425d76
ejguan123 <ej_guan@tk3c.com>
2021-01-19 10:57:32 +0800

    modify image if error change

--- event/js/replace.js
+++ event/js/replace.js
@@ -13,8 +13,6 @@ var location = window.location.pathname.split('/');
         imageUrl = images[i];
         var newImagePath = imageUrl.getAttribute('src');
 
-        //防止不存在圖片在瀏覽器產生錯誤
-        newImagePath.onerror = () => {
         //排除含有cdn網址的圖片
             if (imageUrl.src.indexOf(newUrl) < 0) {
                 if (imageUrl.src.indexOf(imageStr) > -1) {
@@ -24,15 +22,18 @@ var location = window.location.pathname.split('/');
                     }
                 } else {
                     //圖片使用簡短網址: /images/xxxx.png (要到正式機才會抓的到圖片，不然在本機專案資料夾不同會顯示不出來)
-                    if (imageUrl.src.indexOf("https://www.cdn-tkec.tw") < 0 && 
-                        newImagePath.indexOf('/images') > -1 &&
-                        location[2] != undefined) {
-                        var localImage = newImagePath.replace('./images','/images');
-                        imageUrl.src = newUrl + imageStr + '/' + location[2] + localImage;
+
+                    //防止不存在圖片在瀏覽器產生錯誤
+                    imageUrl.onerror = () => {
+                        if (imageUrl.src.indexOf("https://www.cdn-tkec.tw") < 0 && 
+                            newImagePath.indexOf('/images') > -1 &&
+                            location[2] != undefined) {
+                            var localImage = newImagePath.replace('./images','/images');
+                            imageUrl.src = newUrl + imageStr + '/' + location[2] + localImage;
+                        }
                     }
                 }
             }
-        }
     }
 });
 
