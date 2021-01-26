47a06b116d33dd84267985d0e58ca48ce40c4c45
qubtdjy <qubtdjy@gmail.com>
2021-01-27 00:25:10 +0800

    add electro channel js

---
+++ channel/for_electro_page.js
@@ -0,0 +1,12 @@
+$(function(){
+    $('.wrapper').find('.title').append(`
+        <div class="electro-title">
+            <h2><img src="https://events.cdn-tkec.tw/events_net/events_net/201710_nbchannel/images/20210119_text.png">
+            <span class="light"><img src="https://events.cdn-tkec.tw/events_net/events_net/201710_nbchannel/images/20210119_light.png"></span></h2>
+        </div>
+        <div class="boxfor3c">
+            <p class="nb"><img src="https://events.cdn-tkec.tw/events_net/events_net/201710_nbchannel/images/20210119_nb.png"></p>
+            <p class="laplop"><img src="https://events.cdn-tkec.tw/events_net/events_net/201710_nbchannel/images/20210119_PC.png"></p>
+        </div>
+    `);
+});
