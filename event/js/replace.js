d9dff67639ebd5bcafa9f348d18f0c5db0dd33f1
ejguan123 <ej_guan@tk3c.com>
2021-03-16 14:36:19 +0800

    modify replace url

--- event/js/replace.js
+++ event/js/replace.js
@@ -1,6 +1,6 @@
 //活動頁面圖片網址轉換成cdn https://events.cdn-tkec.tw
 
-$(window).load(() => {
+$(function() {
 var images = document.getElementsByTagName('img');
 var imageUrl = '';
 var imageStr = '/events_net/events_net';
@@ -13,9 +13,14 @@ var location = window.location.pathname.split('/');
         var newImagePath = imageUrl.getAttribute('src');
 
         //若圖片不存則連結為空
-        imageUrl.onerror = () => {
-            newImagePath = "";
-            return true;
+        if (imageUrl.naturalWidth === 0) {
+            //圖片使用簡短網址: /images/xxxx.png (要到正式機才會抓的到圖片，不然在本機專案資料夾不同會顯示不出來)
+                if (imageUrl.src.indexOf("https://www.cdn-tkec.tw") < 0 && 
+                newImagePath.indexOf('/images') > -1 &&
+                location[2] != undefined) {
+                var localImage = newImagePath.replace('./images','/images');
+                imageUrl.src = newUrl + imageStr + '/' + location[2] + localImage;
+            }
         }
 
         //排除含有cdn網址的圖片
@@ -24,14 +29,6 @@ var location = window.location.pathname.split('/');
                     imageUrl.src = imageUrl.src.replace(originUrl,newUrl);
                     if (imageUrl.src.indexOf('https://events.tk3c.com.tw') > -1) {
                         imageUrl.src = imageUrl.src.replace('https://events.tk3c.com.tw',newUrl);
-                    }
-                } else {
-                    //圖片使用簡短網址: /images/xxxx.png (要到正式機才會抓的到圖片，不然在本機專案資料夾不同會顯示不出來)
-                    if (imageUrl.src.indexOf("https://www.cdn-tkec.tw") < 0 && 
-                        newImagePath.indexOf('/images') > -1 &&
-                        location[2] != undefined) {
-                        var localImage = newImagePath.replace('./images','/images');
-                        imageUrl.src = newUrl + imageStr + '/' + location[2] + localImage;
                     }
                 }
             }
