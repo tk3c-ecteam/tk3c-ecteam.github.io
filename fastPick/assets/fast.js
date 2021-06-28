296b30d4fc96238bf1d6db63cde8ebfaf46bf839
TK3C\TP030164 <TP030164@tk3c.com>
2021-06-28 14:42:45 +0800

    modify fast

--- fastPick/assets/fast.js
+++ fastPick/assets/fast.js
@@ -1,23 +1,41 @@
 $(function(){
-    $('#fastPickBox').css('overflow-y','auto');
-    $('body').css('overflow-y','hidden');
+
+    if (isMobile() === false) {
+        $('#fastPickBox').css('overflow-y','auto');
+        $('body').css('overflow-y','hidden');
+    }
+
+
+    //選擇門市，彈出訊息視窗(radio)
+    $('#up_StoreShipment input[name="rblGetProductStore"]').on('change',function(){
+        $('#fastPickBox').fadeIn('fast');
+
+        //取得門市radio的值
+        console.log($('input[name="rblGetProductStore"]:checked').val());
+
+        $('#fastPickBox').css('overflow-y','hidden');
+        $('body').css('overflow-y','auto');
+    });
+
+    //選擇門市，彈出訊息視窗(手機版select)
+    $('#ctl00_ContentPlaceHolder1_ddl_AddSystemStore option').on('click',function(){
+        if ($(this).val() != '') {
+            $('#fastPickBox').fadeIn('fast');
+        }
+
+        //取得門市地址 select value值
+        console.log($('#ctl00_ContentPlaceHolder1_ddl_AddSystemStore').val());
+    });
+
 
     $("#fastPickBox #goPick").on('click',function(){
-        let checkRadio = $('#fastPickBox input[type="radio"]');
-
-        //判斷是否有勾選門市
-        if ($(checkRadio).is(':checked')) {
-            $('#fastPickBox').fadeOut();
-            $('#fastPickBox').css('overflow-y','hidden');
-            $('body').css('overflow-y','auto');
-        } else {
-           //alert('請選擇快取門市');
-           $('#alertBox').fadeIn('fast');
-            return false;
-        }
-    });
-
-    $('#alertBox .alertSure').on('click',function(){
-        $('#alertBox').fadeOut('slow');
+        $('#fastPickBox').fadeOut('slow');
     });
 });
+
+//判斷為手機裝置
+function isMobile() {
+    let ua = navigator.userAgent;
+    let isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(ua);
+    return isMobile;
+}
